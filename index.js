71eec804d478d7e3baac5eba5efdd8f2e831bde1
tylermoroz <moroztyler@gmail.com>
2023-11-30 14:58:15 -0700

    Relocate toggle radios to another div. Allow them to change the temperature after the fetch call.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const form = document.querySelector("form");
 const locationInput = document.getElementById("location-input");
 const locationTitle = document.getElementById("location");
+const condition = document.getElementById("condition");
 const temperature = document.getElementById("temp");
+const radios = document.getElementsByName("temperature");
+const feelsLike = document.getElementById("feels-like");
 const celcius = document.getElementById("celcius");
 const farenheit = document.getElementById("farenheit");
 
@@ -11,10 +14,25 @@ const locationData = async () => {
   );
   const response = await apiData.json();
   locationTitle.textContent = `${response.location.name}, ${response.location.region}, ${response.location.country}`;
-  if (farenheit.checked) {
-    temperature.textContent = `${response.current.temp_f}°F`;
-  } else if (celcius.checked) {
-    temperature.textContent = `${response.current.temp_c}°C`;
+  condition.textContent = `${response.current.condition.text}`;
+  temperature.textContent = `${response.current.temp_c}°C`;
+  feelsLike.textContent = `Feels like ${response.current.feelslike_c}°C`;
+  for (let radio of radios) {
+    if (radio.value == "farenheit") {
+      radio.addEventListener("click", () => {
+        temperature.textContent = "";
+        feelsLike.text = "";
+        temperature.textContent = `${response.current.temp_f}°F`;
+        feelsLike.textContent = `Feels like ${response.current.feelslike_f}°F`;
+      });
+    } else if (radio.value == "celcius") {
+      radio.addEventListener("click", () => {
+        temperature.textContent = "";
+        feelsLike.text = "";
+        temperature.textContent = `${response.current.temp_c}°C`;
+        feelsLike.textContent = `Feels like ${response.current.feelslike_c}°C`;
+      });
+    }
   }
   console.log(response);
 };
