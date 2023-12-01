b34fb0becfa0d25f0b6d99d6b1c023f37bf7cf32
tylermoroz <moroztyler@gmail.com>
2023-12-01 15:02:03 -0700

    Apply the checked status to the celcius radio button on render. Delegate first loaded temp settings and adjusted temp settings into 2 different functions.

--- index.js
+++ index.js
@@ -13,28 +13,43 @@ const locationData = async () => {
     `http://api.weatherapi.com/v1/current.json?key=58609b6df0f2402e82c150303231811&q=${locationInput.value}`
   );
   const response = await apiData.json();
-  locationTitle.textContent = `${response.location.name}, ${response.location.region}, ${response.location.country}`;
-  condition.textContent = `${response.current.condition.text}`;
-  temperature.textContent = `${response.current.temp_c}°C`;
-  feelsLike.textContent = `Feels like ${response.current.feelslike_c}°C`;
+  console.log(response);
+  displayData(response);
+  toggleTemp(response);
+};
+
+const displayData = (res) => {
+  locationTitle.textContent = `${res.location.name}, ${res.location.region}, ${res.location.country}`;
+  condition.textContent = `${res.current.condition.text}`;
+  for (let radio of radios) {
+    if (radio.checked && radio.value == "celcius") {
+      temperature.textContent = `${res.current.temp_c}°C`;
+      feelsLike.textContent = `Feels like ${res.current.feelslike_c}°C`;
+    } else if (radio.checked && radio.value == "farenheit") {
+      temperature.textContent = `${res.current.temp_f}°F`;
+      feelsLike.textContent = `Feels like ${res.current.feelslike_f}°F`;
+    }
+  }
+};
+
+const toggleTemp = (res) => {
   for (let radio of radios) {
     if (radio.value == "farenheit") {
       radio.addEventListener("click", () => {
         temperature.textContent = "";
         feelsLike.text = "";
-        temperature.textContent = `${response.current.temp_f}°F`;
-        feelsLike.textContent = `Feels like ${response.current.feelslike_f}°F`;
+        temperature.textContent = `${res.current.temp_f}°F`;
+        feelsLike.textContent = `Feels like ${res.current.feelslike_f}°F`;
       });
     } else if (radio.value == "celcius") {
       radio.addEventListener("click", () => {
         temperature.textContent = "";
         feelsLike.text = "";
-        temperature.textContent = `${response.current.temp_c}°C`;
-        feelsLike.textContent = `Feels like ${response.current.feelslike_c}°C`;
+        temperature.textContent = `${res.current.temp_c}°C`;
+        feelsLike.textContent = `Feels like ${res.current.feelslike_c}°C`;
       });
     }
   }
-  console.log(response);
 };
 
 form.addEventListener("submit", async (event) => {
